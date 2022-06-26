b207b28d462bb2ef0046e509e7f27c01de718994
Camilú <camilu@fabian-VivoBook-ASUSLaptop-X421FAY-X413FA@gmail.com>
2022-06-25 23:41:53 -0400

    refactor(header): change tag img to Image and add tag Link

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,20 @@
 import styles from "../styles/Home.module.css";
 import Search from "./search";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <a href="/">
-        <div>
-          <img src="/labmmba.svg" alt="Logo" />
-          <h1>LabMMBA</h1>
-        </div>
-      </a>
+      <Link href="/">
+        <a>
+          <div>
+            <Image src="/labmmba.svg" alt="Logo" height={45} width={45} />
+            <h1>LabMMBA</h1>
+          </div>
+        </a>
+      </Link>
+
       <form>
         <Search />
       </form>
